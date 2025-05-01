ddd2d5b1a5e3c86cf7d28c247cf1d094f5fe5fd1
Estevão Maia <estevao@programmer.net>
2025-05-01 09:33:33 -0300

    Add frontend/src/components/MapSelector.js 1746102812708

---
+++ frontend/src/components/MapSelector.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+// Fix default icon issue with Leaflet in React
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
+  iconUrl:
+    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
+  shadowUrl:
+    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
+});
+
+function LocationMarker({ position, setPosition }) {
+  useMapEvents({
+    click(e) {
+      setPosition(e.latlng);
+    },
+  });
+
+  return position === null ? null : <Marker position={position}></Marker>;
+}
+
+function MapSelector({ onLocationSelect }) {
+  const [position, setPosition] = useState(null);
+
+  useEffect(() => {
+    if (position) {
+      onLocationSelect(position);
+    }
+  }, [position, onLocationSelect]);
+
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      style={{ height: '300px', width: '100%', marginBottom: '1rem' }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <LocationMarker position={position} setPosition={setPosition} />
+    </MapContainer>
+  );
+}
+
+export default MapSelector;
