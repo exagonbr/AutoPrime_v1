8b2371fcc5f9f23b450e420c0578c9089a698ae5
Estevão Maia <estevao@programmer.net>
2025-05-01 07:24:31 -0300

    Add frontend/src/App.js 1746095070147

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import RequestHelp from './pages/RequestHelp';
+import Mechanics from './pages/Mechanics';
+import Booking from './pages/Booking';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/request-help" element={<RequestHelp />} />
+        <Route path="/mechanics" element={<Mechanics />} />
+        <Route path="/booking" element={<Booking />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
