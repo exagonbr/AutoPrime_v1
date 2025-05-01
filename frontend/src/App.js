f4285f18a52cac50c61a79139041b3d1103ed62e
Estevão Maia <estevao@programmer.net>
2025-05-01 09:33:32 -0300

    Add frontend/src/App.js 1746102811521

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,10 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import RequestHelp from './pages/RequestHelp';
+import RequestHelp from './pages/RequestHelp_temp';
 import Mechanics from './pages/Mechanics';
 import Booking from './pages/Booking';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="/request-help" element={<RequestHelp />} />
         <Route path="/mechanics" element={<Mechanics />} />
         <Route path="/booking" element={<Booking />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
